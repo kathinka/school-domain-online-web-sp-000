f254d6b7983e95e90e79aa88cf81c21984709435
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-01-28 16:53:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+class School
+#  attr_accessor :roster
+attr_reader :add_student
+#roster = {}
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+def roster=(name, grade)
+  if roster.include?(grade) 
+    roster[grade] << name 
+  else
+     roster[grade] = []
+     roster[grade] << name
+   end
+end
+
+def roster
+  @roster 
+end
+
+def add_student=(grade, name)
+  @name = name
+  @grade = grade
+  roster=(name, grade)
+  #if roster.include?(grade) 
+  #  roster[grade] << name 
+  #else
+  #   roster[grade] = []
+  #   roster[grade] << name
+  # end
+end
